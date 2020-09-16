638cb8543036a2e693e9b54091719eceef756177
Marc Espín Sanz <mespinsanz@gmail.com>
2020-09-16 11:59:32 +0200

    :tada: Added Getting started for developers guide

---
+++ docs/dev/starting.md
@@ -0,0 +1,38 @@
+---
+title: 👋🏻 Getting Started
+slug: dev/starting
+
+---
+
+This document instends to be a guide through the different phases for making a plugin.
+
+
+## Fundamentals
+
+Read the fundamentals of how Graviton works.
+
+*  [🧰 .graviton2](fundamentals/graviton2)
+* [🧾 Manifest File](fundamentals/plugins/manifest)
+* [🐧 Puffin](fundamentals/puffin)
+
+You can choose to read the one you prefer.
+* [Dynamic](fundamentals/plugins/dynamic)
+* [IconPack](fundamentals/plugins/iconpacks)
+* [Themes](fundamentals/plugins/themes)
+
+## API
+
+The API documentation is still not fully documented, current docs:
+
+* [RunningConfig](api/runningconfig)
+* [StaticConfig](api/staticconfig)
+* [Dialog](api/dialogs)
+* [Window](api/window)
+* [Environment Inspector](api/env_inspectors)
+* [Explorer](api/explorer)
+
+## SDK
+
+Using Graviton's SDK is optional, but hightly recommended since it makes it all easier.
+
+See more [Here](sdk/about)
--- pages/docs/index.js
+++ pages/docs/index.js
@@ -20,19 +20,22 @@ const Docs = () => {
 				<div className="container">
 					<DocsTitle>Documentation (WIP)</DocsTitle>
 					<div className="markdown-container">
-					<p>Graviton Editor is a cross-platform code editor made using web technologies.</p>
-					<h2>Guides</h2>
-					<LinkCard href="docs/dev/guides/building" >
-						🤠 Building manually
-					</LinkCard>
-					<h2>Fundamentals</h2>
-					<LinkCard href="docs/dev/fundamentals/graviton2" >
-						🧰 .graviton2
-					</LinkCard>
-					<LinkCard href="docs/dev/fundamentals/puffin" >
-						🐧 Puffin
-					</LinkCard>
-				</div>
+						<p>Graviton Editor is a cross-platform code editor made using web technologies.</p>
+						<h2>Guides</h2>
+						<LinkCard href="docs/dev/guides/building" >
+							👋🏻 Getting Started
+						</LinkCard>
+						<LinkCard href="docs/dev/guides/building" >
+							🤠 Building manually
+						</LinkCard>
+						<h2>Fundamentals</h2>
+						<LinkCard href="docs/dev/fundamentals/graviton2" >
+							🧰 .graviton2
+						</LinkCard>
+						<LinkCard href="docs/dev/fundamentals/puffin" >
+							🐧 Puffin
+						</LinkCard>
+					</div>
 				</div>
 			</DocsLayout>
 		</div>
